01f01138a7b48bc5d53c77af718fd5cbed10f372
gurunathan <guru@gegosoft.com>
2026-04-20 21:30:25 +0530

    Added site title, description in church details

--- resources/webbuilder/themes/default/_seo_meta.blade.php
+++ resources/webbuilder/themes/default/_seo_meta.blade.php
@@ -1,15 +1,15 @@
-<title>@yield('title', config('settings.sitetitle', $_church->name ?? config('app.name')))</title>
-<meta name="description" content="@yield('meta_description', config('settings.sitedescription', ''))">
+<title>@yield('title', config('settings.site_title', $_church->name ?? config('app.name')))</title>
+<meta name="description" content="@yield('meta_description', config('settings.site_description', ''))">
 
-<meta property="og:title" content="@yield('title', config('settings.sitetitle', $_church->name ?? config('app.name')))">
-<meta property="og:description" content="@yield('meta_description', config('settings.sitedescription', ''))">
+<meta property="og:title" content="@yield('title', config('settings.site_title', $_church->name ?? config('app.name')))">
+<meta property="og:description" content="@yield('meta_description', config('settings.site_description', ''))">
 <meta property="og:type" content="website">
 @hasSection('og_image')
 <meta property="og:image" content="@yield('og_image')">
 @endif
 
 @if(config('settings.favicon'))
-<link rel="icon" href="{{ \Storage::url(config('settings.favicon')) }}">
+<link rel="icon" href="{{ url(config('settings.favicon')) }}">
 @endif
 
 @stack('meta')
