98131c03f637d5c1e5c39fa3e0225c2013224309
gurunathan <guru@gegosoft.com>
2026-06-03 17:25:04 +0530

    Added user role in grouplink

--- app/Http/Resources/API/GroupLink.php
+++ app/Http/Resources/API/GroupLink.php
@@ -8,45 +8,55 @@ use Illuminate\Support\Facades\Auth;
 class GroupLink extends JsonResource
 {
 
-   /**
-    * Transform the resource into an array.
-    *
-    * @param  \Illuminate\Http\Request  $request
-    * @return array
-    */
-   public function toArray($request)
-   {
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
         $members = GroupLink::where([
-            ['church_id',Auth::user()->church_id],
-            ['user_id','!=',Auth::id()],
-            ['group_id',$this->group_id]
+            ['church_id', Auth::user()->church_id],
+            ['user_id', '!=', Auth::id()],
+            ['group_id', $this->group_id]
         ])->get();
+
+
+
+
+        $grouplinks = GroupLink::where([
+            ['church_id', Auth::user()->church_id],
+            ['user_id', '=', Auth::id()],
+            ['group_id', $this->group_id]
+        ])->first();
+
 
         $group_members = [];
 
-        foreach($members as $member)
-        {
+        foreach ($members as $member) {
             $group_members[] = $member->user->FullName;
         }
 
         $permissions = $this->user->permissionUser;
         $permission_name = [];
-        foreach ($permissions as $permission) 
-        {
+        foreach ($permissions as $permission) {
             $permission_name[] = $permission->permission->display_name;
         }
 
-        return 
-        [
-            'group_id'          => $this->group->id,
-            'group_name'        => $this->group->name,
-            'cover_image'       => $this->group->CoverImagePath,
-            'started'           => date('M-Y',strtotime($this->group->created_at)),
-            'group_category'    => $this->group->groupCategory->name,   
-            'group_type'        => $this->group->group_type,
-            'group_description' => $this->group->description,
-            'user_permissions'  => $permission_name,  
-            'group_members'     => $group_members,
-        ];
+        return
+            [
+                'group_id'          => $this->group->id,
+                'group_name'        => $this->group->name,
+                'cover_image'       => $this->group->CoverImagePath,
+                'started'           => date('M-Y', strtotime($this->group->created_at)),
+                'group_category'    => $this->group->groupCategory->name,
+                'group_type'        => $this->group->group_type,
+                'group_description' => $this->group->description,
+                'user_permissions'  => $permission_name,
+                'group_members'     => $group_members,
+                'role' => $grouplinks->role
+
+            ];
     }
 }
